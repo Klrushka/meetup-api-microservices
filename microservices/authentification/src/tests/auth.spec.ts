ba6bcb6cefda2d49ac4f20336df445da9e054818
Kiryl Sliaptsou <kirillsleptsov12082003@gmail.com>
2022-10-29 20:33:46 +0300

    auth test

--- microservices/authentification/src/tests/auth.spec.ts
+++ microservices/authentification/src/tests/auth.spec.ts
@@ -1,14 +1,11 @@
 import mongoose from 'mongoose';
-import App from '../app';
-import { initRoutes } from '../routes/authentification';
+import {app} from '../app';
 import { mockUser } from './__mock__/data';
 import chai, { assert, expect } from 'chai';
 import chaiHttp from 'chai-http';
 
 
 chai.use(chaiHttp);
-
-const app = new App([initRoutes()]);
 
 describe('authentification test', () => {
 
